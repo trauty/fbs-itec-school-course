0f95ebb319d762ec24bb3f23719ee647e6763c61
etaZetaKaTheta <trautwein.maximilian04@gmail.com>
2022-05-05 13:02:52 +0200

    Lektion 2 jumo, movedir, the legend of bean:the jump jump
    yes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,58 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public Camera cam;
+    public Transform orientation;
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpHeight = 10.0f;
+
+    public float sensitivity = 20.0f;
+    public float movementSpeed = 10.0f;
 
     float keyX, keyY;
 
+    float mouseX, mouseY;
+
+    float xRotation, yRotation;
+
+    Vector3 moveDirection;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     // Update is called once per frame
     void Update()
     {
         keyX = Input.GetAxisRaw("Horizontal");
         keyY = Input.GetAxisRaw("Vertical");
+        moveDirection = keyX * orientation.right + keyY * orientation.forward;
+
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensitivity;
+        xRotation -= mouseY * sensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(xRotation, yRotation, 0.0f));
+        orientation.localRotation = Quaternion.Euler(new Vector3(0.0f, yRotation, 0.0f));
+
+        Jump();
     }
  
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(keyX * 50.0f, 0.0f, keyY * 50.0f));
+        rb.AddForce(moveDirection.normalized * movementSpeed);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.VelocityChange);
+        }
     }
 }
