b44ce555b1c5c793c4307b94942fc09d7e1ca504
trauty <trautwein.maximilian04@gmail.com>
2022-05-24 15:46:30 +0200

    wallrunning fast fertig

--- Assets/Scripts/WallRunning.cs
+++ Assets/Scripts/WallRunning.cs
@@ -8,9 +8,15 @@ public class WallRunning : MonoBehaviour
 
     public float wallDistance = 0.6f;
     public float wallMinJumpHeight = 1.5f;
-    public float wallRunForce = 15.0f;
+    public float wallRunJumpForce = 15.0f;
     public float wallRunGravity = 2.0f;
     public LayerMask wallLayer;
+
+    [Header("Camera Effects")]
+    public Camera cam;
+    public float camTilt = 15.0f;
+    public float camTiltTime = 25.0f;
+    public float tilt = 0.0f;
 
     public Rigidbody rb;
 
@@ -36,8 +42,8 @@ public class WallRunning : MonoBehaviour
 
     private void CheckWall()
     {
-        isWallLeft = Physics.Raycast(transform.position, -transform.right, out wallLeftHit, wallDistance, wallLayer);
-        isWallRight = Physics.Raycast(transform.position, transform.right, out wallRightHit, wallDistance, wallLayer);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.right, out wallLeftHit, wallDistance, wallLayer);
+        isWallRight = Physics.Raycast(transform.position, orientation.right, out wallRightHit, wallDistance, wallLayer);
     }
 
     private bool ReadyToWallRun()
@@ -50,10 +56,40 @@ public class WallRunning : MonoBehaviour
         rb.useGravity = false;
 
         rb.AddForce(Vector3.down * wallRunGravity, ForceMode.Force);
+
+        if (isWallLeft)
+        {
+            tilt = Mathf.Lerp(tilt, -camTilt, camTiltTime * Time.deltaTime);
+        }
+        else if (isWallRight)
+        {
+            tilt = Mathf.Lerp(tilt, camTilt, camTiltTime * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isWallLeft)
+            {
+                Vector3 wallJumpDirection = transform.up + wallLeftHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+                rb.AddForce(wallJumpDirection * wallRunJumpForce, ForceMode.VelocityChange);
+            }
+            else if (isWallRight)
+            {
+                Vector3 wallJumpDirection = transform.up + wallRightHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+                rb.AddForce(wallJumpDirection * wallRunJumpForce, ForceMode.VelocityChange);
+            }
+        }
+
+        isWallRunning = true;
     }
 
     private void StopWallRun()
     {
         rb.useGravity = true;
+        isWallRunning = false;
+
+        tilt = Mathf.Lerp(tilt, 0.0f, camTiltTime * Time.deltaTime);
     }
 }
